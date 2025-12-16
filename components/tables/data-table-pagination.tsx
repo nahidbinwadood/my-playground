bf386d5e7547590359ec2573e3e4d363e77095cc
nahidbinwadood <nahidbinwadood@gmail.com>
2025-12-16 22:44:58 +0600

    fix;

--- components/tables/data-table-pagination.tsx
+++ components/tables/data-table-pagination.tsx
@@ -28,7 +28,7 @@ export function DataTablePagination<TData>({
       </div>
 
       <div className="flex items-center space-x-6 lg:space-x-8">
-        <div className="flex items-center space-x-2">
+        <div className="flex items-center space-x-3">
           <p className="text-sm font-medium">Rows per page</p>
           <Select
             value={`${table.getState().pagination.pageSize}`}
